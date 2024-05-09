6892462f01099a5a26335e5b746f1cdeae0cc257
Dioneer <gulkina_em@mail.ru>
2024-05-10 02:13:28 +0300

    seminar 6

--- src/main/java/Pegas/ApplicationRunner.java
+++ src/main/java/Pegas/ApplicationRunner.java
@@ -2,9 +2,22 @@ package Pegas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApplicationRunner {
+    @Bean
+    public RestTemplate template(){
+        return new RestTemplate();
+    };
+
+    @Bean
+    public HttpHeaders headers()
+    {
+        return new HttpHeaders();
+    }
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
     }
