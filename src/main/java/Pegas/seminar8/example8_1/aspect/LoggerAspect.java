60ee61bd06da081a88056e57e4cabd8b01004276
Dioneer <gulkina_em@mail.ru>
2024-05-21 01:20:13 +0300

    seminar_8 finish

--- src/main/java/Pegas/seminar8/example8_1/aspect/LoggerAspect.java
+++ src/main/java/Pegas/seminar8/example8_1/aspect/LoggerAspect.java
@@ -1,15 +1,20 @@
 package Pegas.seminar8.example8_1.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class LoggerAspect {
     
-    @Around("execution(* Pegas.service.*(..))")
+    @Around("execution(* Pegas.service.*.*(..))")
     public Object log(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         Object returnedByMethod = proceedingJoinPoint.proceed();
         return returnedByMethod;
-    } 
+    }
+    @AfterReturning(value = "@annotation(Tolog)", returning = "returnedValue")
+    public void log(Object returnedValue){
+        System.out.println("Method executed and returned "+returnedValue);
+    }
 }
---
+++ src/main/java/Pegas/seminar8/example8_1/aspect/ToLog.java
@@ -0,0 +1,11 @@
+package Pegas.seminar8.example8_1.aspect;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface ToLog {
+}
